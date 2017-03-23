ae7dbc6fb69b49495a6a166b371ea2bf8d4be454
ThePioneeer <uko_1@hotmail.com>
2017-03-24 01:04:09 +0200

    added simple ajax request for movie search result

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 //SCRIP.JS
+var requestAddress = "http://www.omdbapi.com/?s="
+var response;
+
+document.getElementById("submit-button").addEventListener("click", function(url) {
+    var url = requestAddress + document.getElementById("movie1").value;
+    httpGetAsync(url);
+});
+
+function httpGetAsync(url, callback){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
+             response = JSON.parse(result);
+        }
+    }
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+}
