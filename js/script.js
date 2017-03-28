2a2d3218d7caea2bea7310b3a7b577559ed62879
ThePioneeer <uko_1@hotmail.com>
2017-03-28 21:29:13 +0300

    first sample of working movie search request

--- js/script.js
+++ js/script.js
@@ -1,20 +1,13 @@
 //SCRIP.JS
 //var REQUEST_ADDRESS = "http://www.omdbapi.com/?s=";
 var REQUEST_ADDRESS = "http://www.omdbapi.com/?t=";
-var response;
 var delayTimer;
-
-document.getElementById("submit-button").addEventListener("click", function(url) {
-    var url = REQUEST_ADDRESS + document.getElementById("movie1-input").value;
-    httpGetAsync(url);
-});
 
 document.getElementById("movie1-input").oninput = function(){
     clearTimeout(delayTimer);
     delayTimer = setTimeout(function(){
         var searchUrl = REQUEST_ADDRESS + document.getElementById("movie1-input").value;
-        httpGetAsync(searchUrl);
-        setTitle(response);
+        httpGetAsync(searchUrl, setTitle);
     }, 500);
 };
 
@@ -23,18 +16,18 @@ function httpGetAsync(url, callback){
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
-             response = JSON.parse(xmlHttp.response);
-             console.log("getAsync " + response.Title);
+             callback(JSON.parse(xmlHttp.response));
+             //console.log(xmlHttp.resonse.Title);
         }
     }
     xmlHttp.open("GET", url, true);
     xmlHttp.send();
-    requestComplete = true;
 }
 
 function setTitle(movie){
-    clearTimeout(delayTimer);
-    delayTimer = setTimeout(function(){
-        console.log("setTitle " + movie.Title);
-    }, 1000);    
+    console.log("setTitle " + movie.Title);
+    document.getElementById("movie1-poster").style.backgroundImage = "url('" + movie.Poster + "')";
+    document.getElementById("movie1-rating").innerText = movie.imdbRating;
+    document.getElementById("movie1-name").innerText = movie.Title;
+    document.getElementById("movie1-year").innerText = movie.Year;
 }
