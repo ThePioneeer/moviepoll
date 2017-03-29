1029b320a433a1272e5e2c2cd7a6b5a0e3d7ef35
ThePioneeer <uko_1@hotmail.com>
2017-03-29 13:41:20 +0300

    working movie search on all 3 movies

--- js/script.js
+++ js/script.js
@@ -3,31 +3,39 @@
 var REQUEST_ADDRESS = "http://www.omdbapi.com/?t=";
 var delayTimer;
 
-document.getElementById("movie1-input").oninput = function(){
-    clearTimeout(delayTimer);
-    delayTimer = setTimeout(function(){
-        var searchUrl = REQUEST_ADDRESS + document.getElementById("movie1-input").value;
-        httpGetAsync(searchUrl, setTitle);
-    }, 500);
-};
-
 //GET search result
-function httpGetAsync(url, callback){
+function httpGetAsync(url, callback, id){
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
-             callback(JSON.parse(xmlHttp.response));
-             //console.log(xmlHttp.resonse.Title);
+             callback(JSON.parse(xmlHttp.response), id);
         }
     }
-    xmlHttp.open("GET", url, true);
+    xmlHttp.open("GET", url);
     xmlHttp.send();
 }
 
-function setTitle(movie){
+function findMovie(id) {    
+    clearTimeout(delayTimer);
+    delayTimer = setTimeout(function(){
+        console.log(id);
+        var searchUrl = REQUEST_ADDRESS + document.getElementById(id+ "-input").value;
+        httpGetAsync(searchUrl, setTitle, id);
+    }, 500);
+}
+
+function setTitle(movie, id){
     console.log("setTitle " + movie.Title);
-    document.getElementById("movie1-poster").style.backgroundImage = "url('" + movie.Poster + "')";
-    document.getElementById("movie1-rating").innerText = movie.imdbRating;
-    document.getElementById("movie1-name").innerText = movie.Title;
-    document.getElementById("movie1-year").innerText = movie.Year;
+    if(movie.Title == undefined){
+        document.getElementById(id + "-poster").style.backgroundImage = "";
+        document.getElementById(id + "-rating").innerText = "-"
+        document.getElementById(id + "-name").innerText = "Vali film...";
+        document.getElementById(id + "-year").innerText = "";
+    }else{
+        document.getElementById(id + "-poster").style.backgroundImage = "url('" + movie.Poster + "')";
+        document.getElementById(id + "-rating").innerText = movie.imdbRating;
+        document.getElementById(id + "-name").innerText = movie.Title;
+        document.getElementById(id + "-year").innerText = movie.Year;
+    }
+    
 }
